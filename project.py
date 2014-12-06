ebe355a9ade6254571954065541ff33369e8df1b
Ondrej Jariabka <o.jariabka@gmail.com>
2014-12-06 16:06:51 +0100

    Added detection of edges of a board

--- project.py
+++ project.py
@@ -4,62 +4,52 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
+def getMinMaxIndex(arr):
+	max = arr[0]
+	min = arr[0]
+	maxi = 0
+	mini = 0
+	for i in range(arr.shape[0]):
+		if max < arr[i]:
+			max = arr[i]
+			maxi = i
+		if min > arr[i]:
+			min= arr[i]
+			mini = i
+	return (maxi, mini)
+
 bins = np.arange(256).reshape(256,1)
 
-#def calculateHistogram(image, nameOfWindow="Histogram"):
-#	test = np.zeros((300,256,3))
-#	color = [(255,0,0), (0,255,0), (0,0,255)]
-#	for ch, col in enumerate(color):
-#		hist = cv.calcHist([image], [ch], None, [256], [0, 256])
-#		cv.normalize(hist, hist, 0, 256, cv.NORM_MINMAX)
-#		h = np.int32(np.around(hist))
-#		p = np.int32(np.column_stack((bins, h)))
-#		cv.polylines(test, [p],False, col)
-#	histogram = np.flipud(test)
-#	cv.imshow(nameOfWindow, histogram)
-
-
-def calculateHistogram(image, nameOfWindow="Histogram"):
+def calculateHistogram(image):
 	(width, height, _) = image.shape
-	vlines = [0 for _ in range(width)]
-	hlines = [0 for _ in range(height)]
-
+	vlines = np.zeros(width)
+	hlines = np.zeros(height)
 	for x in range(width):
 		for y in range(height):
 			if 100 < image[x][y][2] < 256:
 				hlines[y] += 1
 				vlines[x] += 1	 
-	
-	plt.figure()
-	plt.plot(hlines)
-	plt.figure()
-	plt.plot(vlines)
-	plt.show()
-########test = np.zeros((300,256,3))
-########color = [(0,255,0)]
-########hist = cv.calcHist([image], [0], None, [256], [0, 256])
-########cv.normalize(hist, hist, 0, 256, cv.NORM_MINMAX)
-########h = np.int32(np.around(hist))
-########p = np.int32(np.column_stack((bins, h)))
-########cv.polylines(test, [p], False, (0, 255, 0))
-########histogram = np.flipud(test)
-########print histogram.shape
-########cv.imshow(nameOfWindow, histogram)
+
+	y = np.diff(vlines)
+	x = np.diff(hlines)
+	return (getMinMaxIndex(x), getMinMaxIndex(y))
 
 def cartoonify(image):
 	out = image.copy()
 	out = cv.medianBlur(image, 5)
-	calculateHistogram(out, "Project Histogram")
-	return out
+	(x, y) = calculateHistogram(out)
+	#draw edges of a board
+	cv.circle(image, (x[1], y[0]), 5, (0, 0 , 255), -1)
+	cv.circle(image, (x[1], y[1]), 5, (0, 0 , 255), -1)
+	cv.circle(image, (x[0], y[0]), 5, (0, 0 , 255), -1)
+	cv.circle(image, (x[0], y[1]), 5, (0, 0 , 255), -1)
 
 def main(inputFile):
 	im = cv.imread(inputFile, cv.CV_LOAD_IMAGE_COLOR)
-	#create output image
-	out = im.copy()
 	#cartoon-ify image
-	out = cartoonify(im)
+	cartoonify(im)
 	#show image
-	cv.imshow("project", out)
+	cv.imshow("project", im)
 	cv.waitKey(0)
 	cv.destroyAllWindows()
 
